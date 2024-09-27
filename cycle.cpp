9d39040b2ef7ac79e3c828b4396497a8f2cf64b7
Anant Mehta <87894541+anantmehta33@users.noreply.github.com>
2024-09-27 17:58:12 -0500

    Create cycle.cpp

---
+++ cycle.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool dfs(int index, vector<int> &vec, vector<int> adj[], int parent)
+    {
+        vec[index] = 1;
+        for(auto x: adj[index])
+        {
+            if(vec[x] == 1 and parent!= x)
+              {
+                  return true;
+              }
+              
+            if(vec[x] == 0)
+            {
+                if(dfs(x,vec,adj,index))
+                  return true;
+            }
+            
+        }
+        return false;
+        
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+
+        vector<int> vec(V,0);
+        bool c = false;
+        for(int i=0;i<V;i++)
+        {
+            if(vec[i] == 0)
+            {
+                c = dfs(i,vec,adj,-2);
+            }
+            if(c == true)
+              return c;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
